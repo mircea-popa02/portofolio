a1bcdbd58eea617fbf2bf2b868bb8988b381cee5
Mircea Popa <pmircea027@gmail.com>
2025-07-31 23:44:29 +0300

    feat: add @vercel/analytics for enhanced performance tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import './i18n'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    <Analytics />
   </BrowserRouter>
 )
